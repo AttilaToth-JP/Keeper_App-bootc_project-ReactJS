55b996d4a133e7a1641b0032b4d7b7120d06d25f
Attila <atomicdevkitten@gmail.com>
2021-03-11 19:48:59 +0000

    starting files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return (
-        <div>
-            <Header />
-            {notes.map( noteEntry =>
-                        <Note 
-                            key={noteEntry.key}
-                            title={noteEntry.title}
-                            content={noteEntry.content}
-                        />
-            )}
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
